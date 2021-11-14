16043eb2629dd591f390b4f2253d252189c7a36b
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-14 16:37:46 -0500

    starting all over

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,12 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Pokemons from "./components/Pokemons";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <Pokemons />
     </div>
   );
 }

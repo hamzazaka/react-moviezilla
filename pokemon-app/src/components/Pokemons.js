13106eb39ff48236fb5c7b562912a4b6ce598aab
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-14 13:31:43 -0500

    working on poke api

--- pokemon-app/src/components/Pokemons.js
+++ pokemon-app/src/components/Pokemons.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Pokemons() {
+    const[pokemon,setPokemon]=useState([])
+    const [pokemonName,setPokemonName]=useState('')
 
     const callPokemon = async()=>{
         const URL= 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=200'
         const response=await fetch(URL);
         const data= await response.json()
-        console.log(data)
-
+        setPokemon(data.results)
+        
     }
+    console.log(pokemon)
     useEffect(()=>{
         callPokemon()
     },[])
     return (
         <div>
+            {pokemon&&
+            <div>
+              <form action="">
+                  <select onChange={(e)=>console.log(e.target.value)} name="" id="">
+                    {pokemon.map((a,idx)=>(
+                    <option key={idx}  value={a.name}>{a.name}</option>
+                    ))}
+                    </select>
+            </form>
             
+
+            </div>
+            
+     }
         </div>
     )
 }

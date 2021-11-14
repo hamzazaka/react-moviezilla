5f6616674ce2a78220b48ec7abeb4348112bdd3a
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-14 14:00:13 -0500

    working on the app

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Pokemons from './components/Pokemons';
+import logo from "./logo.svg";
+import "./App.css";
+import Pokemons from "./components/Pokemons";
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <Pokemons/>
+      <Pokemons />
     </div>
   );
 }
--- pokemon-app/src/components/Pokemons.js
+++ pokemon-app/src/components/Pokemons.js
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Pokemons() {
-    const[pokemon,setPokemon]=useState([])
-    const [pokemonName,setPokemonName]=useState('')
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonName, setPokemonName] = useState("");
 
-    const callPokemon = async()=>{
-        const URL= 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=200'
-        const response=await fetch(URL);
-        const data= await response.json()
-        setPokemon(data.results)
-        
-    }
-    console.log(pokemon)
-    useEffect(()=>{
-        callPokemon()
-    },[])
-    return (
+  const callPokemon = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200";
+    const response = await fetch(URL);
+    const data = await response.json();
+    setPokemon(data.results);
+  };
+  console.log(pokemon);
+  useEffect(() => {
+    callPokemon();
+  }, []);
+  return (
+    <div>
+      {pokemon && (
         <div>
-            {pokemon&&
-            <div>
-              <form action="">
-                  <select onChange={(e)=>console.log(e.target.value)} name="" id="">
-                    {pokemon.map((a,idx)=>(
-                    <option key={idx}  value={a.name}>{a.name}</option>
-                    ))}
-                    </select>
-            </form>
-            
-
-            </div>
-            
-     }
+          <form action="">
+            <select
+              onChange={(e) => setPokemonName(e.target.value)}
+              name=""
+              id=""
+            >
+              {pokemon.map((a, idx) => (
+                <option key={idx} value={a.name}>
+                  {a.name}
+                </option>
+              ))}
+            </select>
+          </form>
+          <h1>{pokemonName}</h1>
         </div>
-    )
+      )}
+    </div>
+  );
 }

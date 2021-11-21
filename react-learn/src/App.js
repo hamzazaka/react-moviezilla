153c5c5481fa5a89c69d292ffb74f6332cb1533d
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-21 16:43:31 -0500

    initial commits onn tis

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>Company Directory</h1>
+     <Employee name='hamza zaka' age={44}/>
+     <Employee name='hamza' age={14}/>
+     <Employee name='noman' age={24}/>
+     <Employee name='arthor' age={54}/>
     </div>
   );
 }
---
+++ react-learn/src/components/Employee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Employee({name,age}) {
+    return (
+        <div>
+            <p>This is an employee and the name is  {name}, and the age is {age} </p>
+        </div>
+    )
+}

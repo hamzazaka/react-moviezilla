cfb464c4226749cfed06c777b74680c240171993
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-03 19:17:10 -0500

    working on the react

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const title='welcome to new blog';
+  const likes=50;
+  const person={name:'hamza'}
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>{title}</h1>
+      <p>{likes} is the lke</p>
+       <p>{person.name} is the number</p>
     </div>
   );
 }

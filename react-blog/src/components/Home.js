60eb6a6299121a457fbc11fca6c39bcbb70acd05
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-06 18:43:37 -0500

    working on the react

--- react-blog/src/components/Home.js
+++ react-blog/src/components/Home.js
@@ -1,16 +1,44 @@
 import React, { useState } from 'react'
 
 export default function Home() {
+    const [click,setClick]=useState([
+        {
+            'name':"hamza zaka",
+              'age':18,
+              'class':6
+        },
+         {
+            'name':"zaka",
+              'age':28,
+              'class':16
+        },
+        {
+            'name':"zaka",
+              'age':28,
+              'class':16
+        },
+         {
+            'name':"zaka",
+              'age':28,
+              'class':16
+        },
+    ])
     
     const handleClick=(name,e)=>{
         console.log('hello world ' +name,e.target)
-        
-
+        setClick(name)
     }
 
     return (
         <div className='home'>
-            <h2>Home page </h2>
+            <h4>{click.map(a=>{
+                return(
+                    <div>
+                    <h4>{a.name}</h4>
+                    <p>{a.age}</p>
+                    </div>
+                )
+            })} </h4>
             <button onClick={(e)=>handleClick('hamza',e)}>click me</button>
         </div>
     )

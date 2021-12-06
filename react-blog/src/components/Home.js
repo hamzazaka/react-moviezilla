2f81696819ff5ca604db7ca0eb912bcb2822b2c9
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-06 18:24:56 -0500

    working on the react

--- react-blog/src/components/Home.js
+++ react-blog/src/components/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
+    const [click,setClick]=useState('')
+    const handleClick=()=>{
+        console.log('hello world')
+        setClick('hamza')
+
+    }
+
     return (
         <div className='home'>
-            <h2>Home page</h2>
+            <h2>Home page {click}</h2>
+            <form action="" onSubmit={handleClick}>
+                <input type="text" placeholder='type your name' id="" />
+                <button >Click</button>
+            </form>
         </div>
     )
 }

979a99ea6fc5ffa5e52418df3b45152836471611
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-07 23:15:22 -0500

    workingon the react

--- react-blog/src/components/Home.js
+++ react-blog/src/components/Home.js
@@ -6,6 +6,8 @@ export default function Home() {
         {title:"my new webstie",body:'hello htis is my ebsie',author:'hamza',id:1},
         {title:"my birthday party",body:'my birthday party went really well and i really enjoyed working there',author:'noman',id:2},
         {title:"software engineer",body:'I am a Front End engineer',author:'mario',id:3},
+        {title:"software engineer",body:'I am a Front End engineer',author:'mario',id:3},
+        {title:"software engineer",body:'I am a Front End engineer',author:'mario',id:3}
     ])
 
     const handleDelete=(id)=>{

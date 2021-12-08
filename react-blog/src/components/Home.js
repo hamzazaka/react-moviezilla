c549436cc46c9bdf6eeb65de5870f457d791f957
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-12-07 23:32:22 -0500

    getting the data

--- react-blog/src/components/Home.js
+++ react-blog/src/components/Home.js
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Blog from './Blog'
 
 export default function Home() {
-    const [blogs,setBlogs]=useState([
-        {title:"my new webstie",body:'hello htis is my ebsie',author:'hamza',id:1},
-        {title:"my birthday party",body:'my birthday party went really well and i really enjoyed working there',author:'noman',id:2},
-        {title:"software engineer",body:'I am a Front End engineer',author:'mario',id:3},
-        {title:"software engineer",body:'I am a Front End engineer',author:'mario',id:3},
-        {title:"software engineer",body:'I am a Front End engineer',author:'mario',id:3}
-    ])
+    const [blogs,setBlogs]=useState([])
 
     const handleDelete=(id)=>{
         const newBlogs=blogs.filter(a=>a.id!==id)
         setBlogs(newBlogs)
         // console.log(id) 
     }
+
+    const getData= async()=>{
+        const blogURL='http://localhost:8000/blogs';
+        const response=await fetch(blogURL)
+        const data=await response.json()
+        console.log(data)
+        setBlogs(data)
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
 
     return (
         <div className='home'>
